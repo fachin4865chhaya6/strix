c0b5d411a231e34c2df8c0c6328842c950367dde
Fachin Chhaya <fachin4865chhaya6@users.noreply.github.com>
2026-04-19 02:51:52 +0800

    expose __author__ and __license__ in __all__ for easier introspection

--- src/strix/__init__.py
+++ src/strix/__init__.py
@@ -10,8 +10,11 @@ __version__ = "0.1.0"
 __author__ = "Strix Contributors"
 __license__ = "MIT"
 
+# Also expose __author__ and __license__ for introspection
 __all__ = [
     "Logger",
     "StrixConfig",
     "__version__",
+    "__author__",
+    "__license__",
 ]
